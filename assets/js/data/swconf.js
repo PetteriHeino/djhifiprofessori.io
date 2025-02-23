99b2040090357fe3d7334df1241e4a72623fa9d6
PetteriHeino <PetteriHeino@users.noreply.github.com>
2025-02-23 22:45:58 +0000

    deploy: 59e4e5f061e13dd8cbac7adbf6cb9c766e887655

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,36 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1740350757',resources: [
+      '/djhifiprofessori.io/assets/css/jekyll-theme-chirpy.css',
+      '/djhifiprofessori.io/',
+      
+        '/djhifiprofessori.io/categories/',
+      
+        '/djhifiprofessori.io/tags/',
+      
+        '/djhifiprofessori.io/archives/',
+      
+        '/djhifiprofessori.io/Images/',
+      
+        '/djhifiprofessori.io/DJ-gigs/',
+      
+        '/djhifiprofessori.io/Live-music/',
+      
+        '/djhifiprofessori.io/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
