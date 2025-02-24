d638bf6bced398fee305ade2c2ed657d1781a078
PetteriHeino <PetteriHeino@users.noreply.github.com>
2025-02-24 15:31:28 +0000

    deploy: 6a88abb6040702b4684928ef365af15708fb75ca

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740409924',resources: [
+    cacheName: 'chirpy-1740411087',resources: [
       '/djhifiprofessori.io/assets/css/jekyll-theme-chirpy.css',
       '/djhifiprofessori.io/',
       
